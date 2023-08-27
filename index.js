028c147a07ea92c48a456fb784707233fa7248a1
Muhammad <huzaifa12345612@gmail.com>
2023-08-27 18:22:48 +0500

    ArrayMethods Assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// ARRAY   M E T H O D S
+
+// const arr1 = [1,3,5,7,9,11];
+
+// console.log("Let Array 1 = ", arr1);
+// let length = arr1.length;                   // returns length of Array
+// console.log("1) Length of Array = ", length);
+
+// arr1.pop();                                // pops the value from the last of Array
+// console.log("2) Array after pop function = ", arr1);
+
+// arr1.push(80);                             // adds value 80 in the last of Array
+// console.log("3) Array after pushing value 80 = ", arr1);
+
+// arr1.shift();                             // removes first element from Array
+// console.log("4) Array after First value being removed using Shift function = ", arr1);
+
+// arr1.unshift(2);                          // adds value 2 at the start of Array
+// console.log("5) Array after adding value 2 in the start using unshift function = ", arr1);
+
+// arr2 =  ['Huzaifa', 'Fatima', 'Ahmed', 'Niazi'];
+// arr3 = [56,34,23,54];
+// arr4 = arr2.concat(arr3);
+// console.log("6) Concatination of Array 2 & Array 3 = ", arr4);
+
+// arr2.splice(2,2, 'Fahad', 'Hafsa');       // Move to index 2 remove two values & add Fahad & Hafsa respectively
+// console.log("7) Array 2 after splice function", arr2);
+
+// const sliced = arr3.slice(1);             // Removes one value from array 
+// console.log("8) Array 3 after slice function = ", sliced);
+
+// const fruits = ['Apple', 'Orange', 'Banana'];
+// let fruitsName = fruits.toString();      // Converts array of fruits into String
+// console.log("9) Conversion of array of fruits to String = ", fruitsName);
+
+// delete fruits[1];                       // Removes element from fruits array at index 1
+// console.log("10) Delete element from index 1 = ", fruits);
+
+// const arr2D = [[1,2], [4,5], [7,8]];
+// let flattedArray = arr2D.flat();       // Converts Multi Dimensional Array to 1 Dimensional Array
+// console.log("11) arr2D after flat function = ",flattedArray);
+
+
+const cities = ['Lahore', 'Karachi'];
+console.log(cities.includes('Karachi'));
+
+
+
