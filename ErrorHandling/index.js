67a47505b79cdccbcc313727479a9b8514793b3c
Muhammad <huzaifa12345612@gmail.com>
2023-08-27 20:22:18 +0500

    Error Handling

---
+++ ErrorHandling/index.js
@@ -0,0 +1,58 @@
+console.log("Error Handling");
+
+// console.log("Ask me a Question");
+// console.log("You Name?");
+// try {    
+//     console.log("Ahmad")                         //try block is for detecting the Error
+//     console.log(Huzaifa);
+// } 
+// catch (error)                    //
+// {
+//     console.log(error+" --> Write String in Qoutations");
+// }
+// console.log("E N D");
+
+
+// let name = 'Riffat';
+// try{
+//     if(name == 'Saba')
+//     {
+//         console.log(5);
+//     }
+//     else{
+//         throw Error('Name is not Fatima');
+//     }
+// }
+// catch(err){
+//     console.log(err);
+// }
+
+
+// try{
+//     console.log(classRoom);
+// }
+// catch(error)
+// {
+//     console.log(error);
+// }
+// finally{                      //finally block will always execute 
+//     console.log("Bunk");
+// }
+
+
+
+// *********************************Synchronous & Asynchronous******************************************************
+
+// console.log("Ahmad")
+// try{
+//     setTimeout(() => console.log(Mujtaba),3000)
+// }
+// catch(err){
+//     console.log(err+"Error");
+// }
+
+// console.log("Karim");
+
+
+
+
